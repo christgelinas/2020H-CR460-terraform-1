984239f9b3faf5e50b61c484a311ad79656597ff
Christian Gélinas <christian@famillegelinas.com>
2021-02-08 21:03:58 -0500

    pratique devoir

--- exercice1.tf
+++ exercice1.tf
@@ -1,12 +1,12 @@
 provider "google" {
-  project = "zonedetest"
+  project = "cr460-devoir1-2054204"
   credentials = "account.json"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
 
 resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
+  name         = "canard"
   machine_type = "f1-micro"
 
   boot_disk {
